c7980ca195e804e34285782374c40822277996f1
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-06-30 17:51:03 +0530

    file name changes

--- b_a_dataprep.py
+++ b_a_dataprep.py
@@ -1,4 +1,5 @@
 import os
+from a_getting_data import image_path
 dir_path = ("/home/ash/FoodVision/data/pizza_steak_sushi")
 
 def walk_through_dir(dir_path):
@@ -7,4 +8,7 @@ def walk_through_dir(dir_path):
     for dirpath, dirnames, filenames in os.walk(dir_path):
         print(f"There are {len(dirnames)} directories and {len(filenames)} images in {dirpath}")
 
-walk_through_dir(dir_path)
+train_dir = image_path / "train"
+test_dir = image_path / "test"
+
+print(train_dir, test_dir)
