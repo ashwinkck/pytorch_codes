a71ba7c4e8aa03f30f1c1e08b966d770a848c41f
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-07-06 14:20:22 +0530

    remove unused data downloading and preparation scripts

--- 1downloading_data.py
+++
@@ -1,6 +0,0 @@
-from getting_data import data
-data_path = (r"/home/ash/FoodVision/data/pizza_steak_sushi")
-with open(data_path / "pizza_steak_sushi.zip", "wb") as f:
-  request = requests.get("https://github.com/mrdbourke/pytorch-deep-learning/raw/main/data/pizza_steak_sushi.zip")
-  print("Downloading Pizza, steak and sushi data..")
-  f.write(request.content)
